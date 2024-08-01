58d82ee3e2e3cc9985489f153f6aaaeabced862e
Cicero <150862765+cicerorph@users.noreply.github.com>
2024-08-01 19:32:44 -0300

    Create tts.js

---
+++ src/routes/gext/tts.js
@@ -0,0 +1,51 @@
+const gTTS = require('gtts');
+
+module.exports = {
+    endpoint: "/tts",
+    method: "get",
+    domain: "gextapi.penguinmod.com",
+    parameters: [
+        {
+            type: "query",
+            required: true,
+            name: "text"
+        },
+        {
+            type: "query",
+            required: false,
+            name: "lang"
+        }
+    ],
+    async execute(c) {
+        const text = c.params.text;
+        if (!text || typeof text !== 'string') {
+            return c.status(400).json({
+                error: "Provide some text",
+                example: "/tts?text=Wow%20so%20cool" // i will let this here bcs why not?
+            });
+        }
+        if (text.length > 512) {
+            return c.status(400).json({
+                error: "Text is too long"
+            });
+        }
+        
+        let lang = c.params.lang;
+        if (!lang || typeof lang !== 'string') {
+            lang = 'en';
+        }
+        
+        let gtts;
+        try {
+            gtts = new gTTS(text, lang);
+        } catch (err) {
+            return c.status(400).json({
+                error: String(err)
+            });
+        }
+        
+        c.status(200);
+        c.setHeader('Content-Type', 'audio/mp3');
+        gtts.stream().pipe(c.res);
+    }
+};
