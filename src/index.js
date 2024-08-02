2cddbdfde9305a5ba841dfaa2359dc72c84bcf30
Cicero <150862765+cicerorph@users.noreply.github.com>
2024-08-02 20:40:13 -0300

    fix cors

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
-const express = require("express");
 const { handleRoutes } = require("./utils/handlers/routeHandle");
-const logger = require("./utils/logger");
 const gconfig = require('../src/configs/general.json');
+const bodyParser = require('body-parser');
+const logger = require("./utils/logger");
+const express = require("express");
+const cors = require('cors');
 
 const app = express();
 const port = gconfig.SERVER_PORT;
 
 app.use(logger);
+app.use(cors({
+    origin: '*',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}));
+app.use(bodyParser.urlencoded({
+    limit: "10kb",
+    extended: false
+}));
+app.use(bodyParser.json({ limit: "10kb" }));
 
 handleRoutes(app);
 
