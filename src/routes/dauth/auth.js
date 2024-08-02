e590cfc2b5fb850265f26a3381baa2e8bbe736aa
Cicero <150862765+cicerorph@users.noreply.github.com>
2024-08-02 09:27:02 -0300

    Atualizar o auth.js

--- src/routes/dauth/auth.js
+++ src/routes/dauth/auth.js
@@ -3,7 +3,7 @@ const config = require('../../configs/dauth.json');
 module.exports = {
     endpoint: "/auth",
     method: "get",
-    domain: "localhost",
+    domain: "discordauth.penguinmod.com",
     parameters: [],
     async execute(c) {
         const authURL = `https://discord.com/api/oauth2/authorize?client_id=${config.CLIENT_ID}&redirect_uri=${encodeURIComponent(config.REDIRECT_URI)}&response_type=code&scope=identify`;
