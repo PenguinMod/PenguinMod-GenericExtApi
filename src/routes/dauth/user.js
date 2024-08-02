cd7a0d26e6c9eb38d71d059a8672b67ef528c30b
Cicero <150862765+cicerorph@users.noreply.github.com>
2024-08-02 09:20:35 -0300

    Atualizar o dauth

---
+++ src/routes/dauth/user.js
@@ -0,0 +1,29 @@
+const config = require('../../configs/dauth.json');
+const userDB = require('../../utils/dauth_db');
+
+module.exports = {
+  endpoint: "/user",
+  method: "get",
+  domain: "localhost",
+  parameters: [{
+    type: "query",
+    required: true,
+    name: "privatecode"
+  }],
+  async execute(c, res) {
+    const privateCode = c.params.privatecode;
+    
+    if (!privateCode) {
+      return c.status(400).send('No private code provided');
+    }
+
+    // Retrieve the user data using the private code from JoshDB
+    const userData = await userDB.get(privateCode);
+
+    if (!userData) {
+      return c.status(404).send('User not found');
+    }
+
+    c.json(userData);
+  }
+}
