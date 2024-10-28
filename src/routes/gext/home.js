4bc8a9f58517f18f4bba9b8a0abc452e6a2e1d58
Cicero <150862765+cicerorph@users.noreply.github.com>
2024-10-28 14:39:45 -0300

    Delete src/routes/gext/home.js

--- src/routes/gext/home.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    endpoint: "/",
-    method: "get",
-    domain: "gextapi.penguinmod.com",
-    parameters: [],
-    async execute(c) {
-      c.status(200).send("what da sigma");
-    }
-};
