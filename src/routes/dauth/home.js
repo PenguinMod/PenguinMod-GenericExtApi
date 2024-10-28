32c09800ea11d7ee96ae2d73da57df3d0bcf05c3
Cicero <150862765+cicerorph@users.noreply.github.com>
2024-10-28 14:39:57 -0300

    Delete src/routes/dauth/home.js

--- src/routes/dauth/home.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    endpoint: "/",
-    method: "get",
-    domain: "discordauth.penguinmod.com",
-    parameters: [],
-    async execute(c) {
-        return c.status(200).send("sigma api is working");
-    }
-}
